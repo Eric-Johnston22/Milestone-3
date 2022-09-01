f01bf1320075c5403543e26a9217182a6a963639
Eric-Johnston <eric.mjohnston@protonmail.com>
2022-09-01 12:14:05 -0400

    added class files

---
+++ Milestone 3/InventoryManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone_3
+{
+    class InventoryManager
+    {
+        // items array, will hold 50 arbitrarily
+        public static Item[] items = new Item[50];
+        public static int itemLength = 50;
+        public static int currentItems = 0;
+
+        public void DisplayItems()
+        {
+            for (int i = 0; i < currentItems; i++)
+            {
+                Console.WriteLine(items[i].itemName + " " + items[i].qty);
+            }
+        }
+
+        public void AddItem(string itemName, int qty, string size)
+        {
+            items[currentItems] = new Item(itemName, qty, size);
+            currentItems++;
+        }
+
+        public void DeleteItem(int index)
+        {
+            for (int i = index; i < currentItems; i++)
+            {
+                items[i] = items[i + 1];
+            }
+            currentItems--;
+        }
+
+        public void RestockItem(int index, int qty)
+        {
+            for (int i = index; i < currentItems; i++)
+            {
+                items[index].qty = qty;
+            }
+
+        }
+
+        public void ItemSearch(string name, int qty)
+        {
+            for (int i = 0; i < currentItems; i++)
+            {
+                if (items[i].itemName == name)
+                {
+                    Console.WriteLine("Found item by name: " + items[i].itemName + " | Qty: " + items[i].qty);
+                }
+                if (items[i].qty == qty)
+                {
+                    Console.WriteLine("Found item by quantity: " + items[i].itemName + " | Qty: " + items[i].qty);
+                }
+            }
+        }
+    }
+}
---
+++ Milestone 3/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone_3
+{
+    public class Item
+    {
+        public string itemName;
+        public int qty;
+        public string size;
+
+        // Constructor with parameters
+        public Item(string itemName, int qty, string size)
+        {
+            this.itemName = itemName;
+            this.qty = qty;
+            this.size = size;
+        }
+    }
+}
