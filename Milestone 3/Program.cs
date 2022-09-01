76daf26155a4afee0057410478ad10bf4d734897
Eric-Johnston <eric.mjohnston@protonmail.com>
2022-09-01 11:56:56 -0400

    fixed array

--- Milestone 3/Program.cs
+++ Milestone 3/Program.cs
@@ -14,182 +14,116 @@ namespace Milestone_3
 
             Console.WriteLine("Adding items to inventory...\n");
             // Add 4 items to inventory
-            inventoryManager.AddItem("Hiking shoe", 3, "shoe", 9);
-            inventoryManager.AddItem("Tent", 3, "3 season", 2);
-            inventoryManager.AddItem("Pants", 5, "pants", 3);
-            inventoryManager.AddItem("Shirt", 7, "shirt", 7);
+            inventoryManager.AddItem("Hiking shoe", 3, "9");
+            inventoryManager.AddItem("Tent", 3, "2 person");
+            inventoryManager.AddItem("Pants", 5, "Large");
+            inventoryManager.AddItem("Shirt", 7, "Medium");
 
             // Display inventory
             inventoryManager.DisplayItems();
 
-            Console.WriteLine("----------------------------------------------------------------------");
+            Console.WriteLine("\n----------------------------------------------------------------------");
 
             // Await keystroke to continue
             Console.ReadKey();
 
-            Console.WriteLine("\nType item name to remove from inventory: \n");
+            Console.WriteLine("\nEnter item index to remove from inventory: ");
             //Remove Pants from inventory
-            inventoryManager.DeleteItem(Console.ReadLine());
+            inventoryManager.DeleteItem(int.Parse(Console.ReadLine()));
 
+            Console.WriteLine("\n");
             // Display inventory after removing item
             inventoryManager.DisplayItems();
 
-            Console.WriteLine("----------------------------------------------------------------------");
+            Console.WriteLine("\n----------------------------------------------------------------------\n");
 
             // Restock item
-            Console.WriteLine("\nEnter an item name and new stock amount: ");
-            inventoryManager.RestockItem(Console.ReadLine(), Int32.Parse(Console.ReadLine()));
+            Console.WriteLine("\nEnter item index and new stock amount: ");
+            inventoryManager.RestockItem(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
             Console.WriteLine("\n");
 
             // Display inventory after restocking item
             inventoryManager.DisplayItems();
 
-            Console.WriteLine("----------------------------------------------------------------------");
+            Console.WriteLine("\n----------------------------------------------------------------------\n");
 
             // Await keystroke to continue
             Console.ReadKey();
 
             Console.WriteLine("\nSearch inventory for an item, by name, quantity or both: ");
-            inventoryManager.ItemSearch(Console.ReadLine(), Int32.Parse(Console.ReadLine()));
+            inventoryManager.ItemSearch(Console.ReadLine(), int.Parse(Console.ReadLine()));
             Console.WriteLine("\n");
 
             // Await keystroke to continue
             Console.ReadKey();
         }
 
-        public static List<Item> ItemList = new List<Item>();
-
-        // Parent item class
-        abstract public class Item
+        // Item class
+        public class Item
         {
             public string itemName;
             public int qty;
-
-            // Return item type
-            public abstract string getType();
-
-            // Constructor with parameters
-            public Item(string itemName, int qty)
-            {
-                this.itemName = itemName;
-                this.qty = qty;
-            }
-
-            public string DisplayName
-            {
-                get
-                {
-                    return itemName;
-                }
-            }
-        }
-
-        class Footwear : Item
-        {
-            public string type;
-            public double size;
-
-
-            // Constructor with parameters
-            public Footwear(string itemName, int qty, string type, double size) : base(itemName, qty)
-            {
-                this.type = type;
-                this.size = size;
-            }
-
-            // Return item type
-            public override string getType()
-            {
-                return type;
-            }
-        }
-
-        class Softgoods : Item
-        {
-            public string type;
             public string size;
 
             // Constructor with parameters
-            public Softgoods(string itemName, int qty, string type, string size) : base(itemName, qty)
+            public Item(string itemName, int qty, string size)
             {
-                this.type = type;
+                this.itemName = itemName;
+                this.qty = qty;
                 this.size = size;
-            }
-
-            // Return item type
-            public override string getType()
-            {
-                return type;
-            }
-        }
-
-        class Hardgoods : Item
-        {
-            public string type;
-            public double size;
-
-            // Constructor with parameters
-            public Hardgoods(string itemName, int qty, string type, double size) : base(itemName, qty)
-            {
-                this.type = type;
-                this.size = size;
-            }
-
-            // Return item type
-            public override string getType()
-            {
-                return type;
             }
         }
 
         class InventoryManager
         {
+            // items array, will hold 50 arbitrarily
+            public static Item[] items = new Item[50];
+            public static int itemLength = 50;
+            public static int currentItems = 0;
+
             public void DisplayItems()
             {
-                for (int i = 0; i < ItemList.Count; i++)
+                for (int i = 0; i < currentItems; i++)
                 {
-                    Console.WriteLine(ItemList[i].itemName + " | Qty: " + ItemList[i].qty);
+                    Console.WriteLine(items[i].itemName + " " + items[i].qty);
                 }
             }
 
-            public void AddItem(string itemName, int qty, string type, double size)
+            public void AddItem(string itemName, int qty, string size)
             {
-                ItemList.Add(new Footwear(itemName, qty, type, size));
+                items[currentItems] = new Item(itemName, qty, size);
+                currentItems ++;
             }
 
-            public void DeleteItem(string name)
+            public void DeleteItem(int index)
             {
-                for (int i = 0; i < ItemList.Count; i++)
+                for (int i = index; i < currentItems; i++)
                 {
-                    if (ItemList[i].itemName == name)
-                    {
-                        ItemList.Remove(ItemList[i]);
-                    }
+                    items[i] = items[i + 1];
                 }
+                currentItems --;
             }
 
-            public void RestockItem(string name, int qty)
+            public void RestockItem(int index, int qty)
             {
-                for (int i = 0; i < ItemList.Count; i++)
+                for (int i = index; i < currentItems; i++)
                 {
-                    if (ItemList[i].itemName == name)
-                    {
-                        ItemList[i].qty = qty;
-                    }
+                    items[index].qty = qty;
                 }
+                
             }
 
             public void ItemSearch(string name, int qty)
             {
-                for (int i = 0; i < ItemList.Count; i++)
+                for (int i = 0; i < currentItems; i++)
                 {
-                    if (ItemList[i].itemName == name)
+                    if (items[i].itemName == name)
                     {
-                        Console.WriteLine("Found item by name: " + ItemList[i].itemName + " | Qty: " + ItemList[i].qty);
+                        Console.WriteLine("Found item by name: " + items[i].itemName + " | Qty: " + items[i].qty);
                     }
-                    if (ItemList[i].qty == qty)
+                    if (items[i].qty == qty)
                     {
-                        Console.WriteLine("Found item by quantity: " + ItemList[i].itemName + " | Qty: " + ItemList[i].qty);
+                        Console.WriteLine("Found item by quantity: " + items[i].itemName + " | Qty: " + items[i].qty);
                     }
                 }
             }
